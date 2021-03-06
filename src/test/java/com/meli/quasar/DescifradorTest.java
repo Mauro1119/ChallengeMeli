da06ea4090e4eb3c58a1e8e763f6cafbbd6f97d6
mauromarozzi <mauro.marozzi@limansky.com>
2021-03-06 08:23:53 -0300

    Entregable

--- src/test/java/com/meli/quasar/DescifradorTest.java
+++ src/test/java/com/meli/quasar/DescifradorTest.java
@@ -20,33 +20,88 @@ public class DescifradorTest {
 	@Parameters
 	public static Iterable<Object[]> getData() {
 
-		Satellites sats = new Satellites();
-		Satellite sat = new Satellite();
-		sat.setName("Kenobi");
-		sat.setDistance(0);
-		sat.setMessage(new String[]{"este","","","mensaje","",""});
-		sats.addSatellite(sat);
-		
-		sat.setName("Skywalker");
-		sat.setDistance(0);
-		sat.setMessage(new String[]{"","es","","","ultra",""});
-		
-		sat.setName("Sato");
-		sat.setDistance(0);
-		sat.setMessage(new String[]{"","","un","","","secreto"});
-		
+		return Arrays.asList(new Object[][] {
+				{ GenerarSatellites(NuevoSatellite("Kenobi", 0, new String[] { "este", "", "", "mensaje", "", "" }),
+						NuevoSatellite("Skywalker", 0, new String[] { "este", "", "", "mensaje", "", "" }),
+						NuevoSatellite("Sato", 0, new String[] { "este", "", "", "mensaje", "", "" })), null },
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 565.68,
+								new String[] { "este", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 360.55,
+								new String[] { "este", "", "un", "mensaje", "", "" }),
+						NuevoSatellite("Sato", (float) 608.27,
+								new String[] { "este", "", "", "mensaje", "ultra", "" })),
+						RespuestaEsperada(new float[] { -100, 200 }, "este es un mensaje ultra secreto") },
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 1085.41,
+								new String[] { "", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 626.99,
+								new String[] { "", "", "un", "mensaje", "", "" }),
+						NuevoSatellite("Sato", (float) 403.88,
+								new String[] { "", "", "", "mensaje", "ultra", "" })), null},
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 565.68,
+								new String[] { "este", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 360.55,
+								new String[] { "este", "", "un", "mensaje", "", "" })),	null},
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 100,
+								new String[] { "este", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 105.5,
+								new String[] { "este", "", "un", "mensaje", "", "" }),
+						NuevoSatellite("Sato", (float) 142.7,
+								new String[] { "este", "", "", "mensaje", "ultra", "" })),null },
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 583.09,
+								new String[] { "este", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 412.31,
+								new String[] { "este", "", "un", "mensaje", "", "" }),
+						NuevoSatellite("Sato", (float) 781.02,
+								new String[] { "este", "", "", "mensaje", "ultra", "" })),
+						RespuestaEsperada(new float[] { 0, -500 }, "este es un mensaje ultra secreto") },
+				{ GenerarSatellites(
+						NuevoSatellite("Kenobi", (float) 565.68,
+								new String[] { "este", "es", "", "mensaje", "", "secreto" }),
+						NuevoSatellite("Skywalker", (float) 360.55,
+								new String[] { "este", "", "un", "mensaje", "", "" }),
+						NuevoSatellite("Sato", (float) 608.27,
+								new String[] { "este", "", "", "mensaje", "ultra", "" }),
+						NuevoSatellite("Otro", (float) 608.27,
+								new String[] { "este", "", "", "mensaje", "ultra", "" })),
+						RespuestaEsperada(new float[] { -100, 200 }, "este es un mensaje ultra secreto")}});
+
+	}
+
+	private static ResponseDecoded RespuestaEsperada(float[] coord, String mensaje) {
 		ResponseDecoded resp = new ResponseDecoded();
 		Position pos = new Position();
-		pos.setXY(new float[] {});
-		resp.setMessage("");
+		pos.setXY(coord);
+		resp.setMessage(mensaje);
 		resp.setPos(pos);
-		
-		return Arrays.asList(new Object[][] {
-				{sats, null}			
-		});
+
+		return resp;
 	}
-	
-	
+
+	private static Satellite NuevoSatellite(String nombre, float distancia, String[] mensaje) {
+		Satellite sat = new Satellite();
+		sat.setName(nombre);
+		sat.setDistance(distancia);
+		sat.setMessage(mensaje);
+
+		return sat;
+	}
+
+	private static Satellites GenerarSatellites(Satellite... sat) {
+		Satellites sats = new Satellites();
+
+		for (int i = 0; i < sat.length; i++) {
+			sats.addSatellite(sat[i]);
+		}
+
+		return sats;
+
+	}
+
 	private Satellites sat;
 	private ResponseDecoded resp;
 
@@ -59,7 +114,15 @@ public class DescifradorTest {
 	public void testProcesarInfo() {
 		ResponseDecoded respuesta = Descifrador.ProcesarInfo(sat);
 
-		assertEquals(resp, respuesta);
+		if (resp != null) {
+
+			assertEquals(resp.getPos().getX(), respuesta.getPos().getX(), 0.05);
+			assertEquals(resp.getPos().getY(), respuesta.getPos().getY(), 0.05);
+			assertEquals(resp.getMessage(), respuesta.getMessage());
+		} else {
+
+			assertEquals(resp, respuesta);
+		}
 
 	}
 
